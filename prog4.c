4f607604df71c44cee6b25cc94faa009a6ae7f6a
lalitha530 <61489607+lalitha530@users.noreply.github.com>
2020-02-26 11:18:47 +0530

    Add files via upload
    sample c programs

---
+++ prog4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a=10,b=4;
+if(a>b)
+printf("a is greater than b\n");
+else
+printf("a is less than or equal to b\n");
+if(a>=b)
+printf("a is greater than or equal to b\n");
+ 
